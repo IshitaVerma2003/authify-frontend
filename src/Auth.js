204a62f6f9b8f372b3181f791b8192dc7865391d
ISHITA VERMA <ishitaverma@ISHITAs-MacBook-Air-2.local>
2026-06-02 22:18:04 +0530

    Fix backend URL

--- src/Auth.js
+++ src/Auth.js
@@ -1,12 +1,14 @@
-const API_URL = "http://localhost:8080";
+const API_URL =
+  process.env.REACT_APP_API_URL ||
+  "https://authify-backend-0ilf.onrender.com";
 
 export const loginUser = async (data) => {
   const response = await fetch(`${API_URL}/login`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   });
 
   return response.json();
@@ -16,9 +18,9 @@ export const registerUser = async (data) => {
   const response = await fetch(`${API_URL}/register`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   });
 
   return response.json();
